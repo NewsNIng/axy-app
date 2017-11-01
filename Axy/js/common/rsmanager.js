288d09aeeea9d8e17a6de3d6790d54dcfdea6b62
newsning@sina.com <newsning@sina.com>
2017-11-01 08:18:39 +0800

    完善热更新功能，待测试

--- Axy/js/common/rsmanager.js
+++ Axy/js/common/rsmanager.js
@@ -3,6 +3,14 @@
  * 资源管理
  */
 (function(w){
+	var debug = true;
+	var log = function(msg, toStr){
+		if(debug){
+			msg = toStr? JSON.stringify(msg): msg;
+			console.log("[资源管理]" + msg);
+		}
+	}
+	
 	
 	var m = {};
 	/**
@@ -13,13 +21,16 @@
 		return /(wgt|wgtu|apk)$/g.test(uri);
 	};
 	
-	/**
-	 * 安装新资源
-	 * @param {String} uri 资源地址
-	 * @param {Function} callback 下载完成回调函数
-	 * @param {Boolean} isHide 是否隐藏下载
-	 */
-	m.install = function(uri, callback, isHide){
+	function _showUpdatePrompt(title, message, callback){
+		plus.nativeUI.confirm(message || "是否更新", function(e){
+			callback(e.index === 0);
+		}, {
+			title: title || "提示",
+			buttons: ["立即更新", "取消"]
+		})
+	}
+	
+	function _install(uri, callback, isHide){
 		var w = {
 			close: function(){},
 			setTitle: function(){}
@@ -32,7 +43,7 @@
 				callback(false);
 			}
 		};
-		var task = plus.downloader.createDownload(uri, {method: "GET"}, function(d, status) {
+		var task = plus.downloader.createDownload(uri, {method: "GET", timeout: 10}, function(d, status) {
 			// 下载成功
 			if(status === 200) {
 				// 转换路径
@@ -40,6 +51,7 @@
 				if(path.indexOf('file') < 0) {
 					path = 'file://' + path;
 				}
+				log("" + path);
 				// 开始安装
 				w.setTitle("开始安装...");
 				plus.runtime.install(path, {
@@ -51,10 +63,11 @@
 						// 未重启则提示
 						plus.nativeUI.toast('您需要手动关闭应用并重启');
 					});
-					result(true);
-				},function(){
+					result(true, path);
+				},function(e){
+					log(e, true);
 					// 安装失败
-					result(false);
+					result(false, e);
 				});
 				
 			} else {
@@ -83,6 +96,58 @@
 		// 开始下载
 		task.start();
 		return task;
+	}
+	
+	/**
+	 * 安装新资源
+	 * @param {String} uri 资源地址
+	 * @param {Function} callback 下载完成回调函数
+	 * @param {Object} options 参数配置
+	 */
+	m.install = function(uri, callback, options){
+		
+		options = (function(o, _o){
+			for(var k in _o){
+				o[k] = _o[k];
+			}
+			return o;
+		}({
+			hide: false, // 是否隐藏更新
+			show: true, // 是否提示
+			force: false, // 强制更新 关不了提示框
+			title: "", // 更新提示框的标题内容 可以设置为app的版本号
+			message: "是否现在更新", // 提示框显示内容，可以设置为app新版内容功能讲解  \n换行
+		}, options));
+		
+		var isHide = options.hide;
+		
+		var that = this;
+		
+		if(isHide){
+			options.show = false;
+		}
+		
+		if(options.show){
+			_showUpdatePrompt(options.title, options.message, function _showUpdatePromptCallback(succ){
+				// 点击了取消更新
+				if(!succ){
+					// 如果是强制更新
+					if(options.force){
+						// 递归调用再次提示
+						plus.nativeUI.toast("应用旧版本将无法使用，请立即更新。");
+						return _showUpdatePrompt(options.title, options.message, _showUpdatePromptCallback);
+					}
+				}else{
+					// 执行更新
+					_install.call(that, uri, callback, isHide);	
+				}
+				
+						
+			});
+		}
+		
+		
+		
 	};
 	
 	/**
