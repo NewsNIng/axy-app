4cfa68e99a4ef775363d8a02798a2919ac5b26a5
curtain <1778870996@qq.com>
2018-01-12 16:51:53 +0800

    -

--- Axy/js/app/app-dev.js
+++ Axy/js/app/app-dev.js
@@ -5,20 +5,24 @@
 		"WG-100": 0x80, // 智能网关
 		"AX-903": 0x17, // 电力主机
 		"AX-904": 0x90, // GPRS报警主机
+		"VH-104MG": 0x1044, // 4路混合DVR
+		"VH-104DG": 0x1045, // 4路DVR
+		"VH-104GN": 0x1046, // 4路NVR
+
 	};
 
 	var _isEqual = function(name, type) {
 		return type & (~(0x01 << 31)) === absTypeidDir[name];
 	};
-	
+
 	dev.get0xType = function(name) {
 		return absTypeidDir[name];
 	};
-	
-	dev.findName = function(type){
+
+	dev.findName = function(type) {
 		type = type & (~(0x01 << 31));
-		for(var i in absTypeidDir){
-			if(absTypeidDir[i] === type){
+		for(var i in absTypeidDir) {
+			if(absTypeidDir[i] === type) {
 				return i;
 			}
 		}
@@ -34,10 +38,12 @@
 		}
 		return
 	}
-	
+
 	// 修复设备名称
-	dev.fixName = function(name){
-		if(!name){return ""}
+	dev.fixName = function(name) {
+		if(!name) {
+			return ""
+		}
 		return name.replace(/<[^<]*>/gi, "");
 	}
 
