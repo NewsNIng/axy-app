bcaa39ce3cd9d2a92cc4e4a8f5210388ac801ee0
newsning@sina.com <newsning@sina.com>
2018-06-12 10:57:20 +0800

    iOS GPRS 版本测试代码 216

--- wwwdev/js/components/home/warnmsg.js
+++ wwwdev/js/components/home/warnmsg.js
@@ -1,16 +1,28 @@
 Vue && Vue.component('warn-msg', {
 
-	template: '<div class="home-warnmsg" v-if="items.length" ><img src="../../image/home/icon_Unreadmessage@3x.png" /><transition-group name="flip-list" tag="div"><div class="home-warnmsg-item" v-for="o,i in items" :key="o.ID" @tap="onTap(o)"><span class="app-font-size-26 mui-ellipsis">{{_fixDevLocation(o.location) +　" " + (o.accessoryName || o.accessoryID)}}</span><span class="app-font-size-26 home-warnmsg-item-right">{{_fixTimeAgo(o.ntime)}}</span></div></transition-group></div>',
+	template: '<div class="home-warnmsg" v-if="items.length" ><img src="../../image/home/icon_Alarmnews@3x.png" /><transition-group name="flip-list" tag="div"><div class="home-warnmsg-item" v-for="o,i in items" :key="o.id" @tap="onTap(o)"><span class="app-font-size-26 mui-ellipsis">{{_fixDevLocation(o.location) +　" " + (o.areaname || o.areaid)}}</span><span class="app-font-size-26 home-warnmsg-item-right">{{_fixTimeAgo(o.atime)}}</span></div></transition-group></div>',
 
 	data: function() {
 		return {
 			animate: false,
-			items: [],
+			items: [
+
+			],
 			st: null,
 		}
 	},
-	plusReady: function() {
+	created: function() {
 		var that = this;
+		// 设备就绪流
+		var pready$ = Rx.Observable.create(function(ob) {
+			if(window.plus) {
+				ob.next();
+			} else {
+				document.addEventListener('plusready', function() {
+					ob.next();
+				});
+			}
+		});
 
 		var NotifyWarningMsg$ = Rx.Observable.create(function(ob) {
 			// 原生通知告警信息刷新
@@ -18,33 +30,29 @@ Vue && Vue.component('warn-msg', {
 				ob.next(data);
 			});
 		});
+		
+		
 
-		var Listen$ = Rx.Observable.create(function(ob) {
-			_B.on('login_success', function() {
-				ob.next();
-			});
-			_B.on('home_reload', function() {
-				ob.next();
-			});
-			plus.webview.currentWebview().addEventListener('show', function(){
-				ob.next();
-			});
+
+		Rx.Observable.create(function(ob) {
+				_B.on('login_success', function() {
+					ob.next();
+				});
+				_B.on('home_reload', function() {
+					ob.next();
+				});
+
+				app.user.has() && ob.next();
+			})
 			
-			new ni.Broadcast().on('update_warn_message',function(){
-				ob.next();
+			.merge(pready$.mergeMapTo(NotifyWarningMsg$).debounceTime(3e3))
+		
+			.mergeMap(function() {
+				return Rx.Observable.fromPromise(that.getMessageList())
 			})
 
-			app.user.has() && ob.next();
-		});
-		
-		
-
-		Listen$.merge(NotifyWarningMsg$.debounceTime(3e3))
-
-			.mergeMap(that.getMessageList)
-
 			.subscribe(function(data) {
-
+				
 				that.items = data;
 
 				if(data.length > 1) {
@@ -62,38 +70,28 @@ Vue && Vue.component('warn-msg', {
 			});
 
 	},
+	
 
 	methods: {
 		// 获取告警列表
-		//		getMessageList: function() {
-		//			return new Promise(function(resolve, reject) {
-		//				dal.message.getAlarmList(1, "", "", function(err, data) {
-		//
-		//					if(err) {
-		//						return reject(err);
-		//					}
-		//					if(!data || data.length === 0) {
-		//						return;
-		//					}
-		//					resolve(data);
-		//				});
-		//			});
-		//
-		//		},
-
 		getMessageList: function() {
-			return Rx.Observable.create(function(ob){
-				var username = app.user.get().account;
-				plug.H5NativeBridge.GetNoReadAlarmListAsyn(username, 10, function(data) {
-					data = JSON.parse(data);
-					if(data.code != 0) return;
-					data = data.data;
-					ob.next(data);
-				})
+			return new Promise(function(resolve, reject) {
+				dal.message.getAlarmList(1, "", "", function(err, data) {
+
+					if(err) {
+						return reject(err);
+					}
+					if(!data || data.length === 0) {
+						return;
+					}
+					resolve(data);
+				});
 			});
+
 		},
+
 		onTap: function(o) {
-			mui.openWindow('../person/message/index.html', "message_center");
+			mui.openWindow('../person/message/index.html');
 		},
 
 		scroll: function() {
@@ -109,7 +107,7 @@ Vue && Vue.component('warn-msg', {
 			return app.dev.fixName(s);
 		},
 		_fixTimeAgo: function(s) {
-			s = s * 1000;
+			s = s.replace(/-/g, "/");
 			return new Date().ago(s);
 		},
 	},
