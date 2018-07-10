048d41e13bedefce0021775a977483193e07d9b9
wangminglmm <wm15171450201>
2018-07-10 14:09:43 +0800

    '修复bug'

--- Axy/js/plug/PlugH5NativeBridge.js
+++ Axy/js/plug/PlugH5NativeBridge.js
@@ -22,14 +22,14 @@
 
 	pg.asyncExec = function(_BARCODE, _METHODNAME, _ARGARR, _SFN, _FFN) {
 		//plus.nativeUI.toast(_METHODNAME);// console.log(_METHODNAME);
-	 	 return;
+//	 	 return;
 		_ARGARR.unshift(pg.getCallBackId(_SFN, _FFN));
 		return B.exec(_BARCODE, _METHODNAME, _ARGARR);
 	}
 
 	pg.syncExec = function(_BARCODE, _METHODNAME, _ARGARR) {
 		//plus.nativeUI.toast(_METHODNAME);
-		 return;
+//		 return;
 		return B.execSync(_BARCODE, _METHODNAME, _ARGARR);
 	}
 
