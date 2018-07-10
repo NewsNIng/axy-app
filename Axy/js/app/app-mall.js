497a163a903d5103d1c8d9536a29727dedbae581
wangminglmm <wm15171450201>
2018-07-10 10:58:33 +0800

    fixedbug

--- Axy/js/app/app-mall.js
+++ Axy/js/app/app-mall.js
@@ -22,8 +22,8 @@
 	// 配置	
 	var config = {
 //		url: "http://47.106.92.195/shop/#/home?t=" + +new Date(),
-//		url: "http://192.168.1.135:8080/#/home?t=" + +new Date(),
-		url: "http://vh.anxin-net.com/shop/#/home?t=" + +new Date(),
+		url: "http://192.168.1.135:8080/#/home?t=" + +new Date(),
+//		url: "http://vh.anxin-net.com/shop/#/home?t=" + +new Date(),
 		id: "mall",
 		routerCmdName: "router",
 	};
